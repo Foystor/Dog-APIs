b1c05a520c0becb77b9bdfaf94b130394a73a76f
Foystor <foystor@gmail.com>
2022-07-31 03:56:21 +0800

    feat: Create a project for DogMicroservice

---
+++ DogMicroservice/dogs/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO DOG (id, name, breed, origin) VALUES (1, '小白','科基','亚洲');
+INSERT INTO DOG (id, name, breed, origin) VALUES (2, '小黑','金毛','欧洲');
+INSERT INTO DOG (id, name, breed, origin) VALUES (3, '小红','吉娃娃','南美洲');
+INSERT INTO DOG (id, name, breed, origin) VALUES (4, '小蓝','雪纳瑞','北美洲');
+INSERT INTO DOG (id, name, breed, origin) VALUES (5, '小黄','斑点','澳洲');
