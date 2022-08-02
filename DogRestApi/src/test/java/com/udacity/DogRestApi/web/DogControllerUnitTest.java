05de113a9ccc873e228298f3adc15e1c57807e9f
Foystor <foystor@gmail.com>
2022-08-02 10:22:48 +0800

    chore: Remove unneeded import

--- DogRestApi/src/test/java/com/udacity/DogRestApi/web/DogControllerUnitTest.java
+++ DogRestApi/src/test/java/com/udacity/DogRestApi/web/DogControllerUnitTest.java
@@ -12,8 +12,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Map;
-
 import static org.mockito.Mockito.verify;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
